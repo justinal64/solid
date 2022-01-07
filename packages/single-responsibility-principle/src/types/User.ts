efafeafc56e9809d91ea2778f0cd24d96f7e4050
Justin Leggett <justin.leggett@daveramsey.com>
2022-01-07 07:15:42 -0600

    removed extra info from User type

--- packages/single-responsibility-principle/src/types/User.ts
+++ packages/single-responsibility-principle/src/types/User.ts
@@ -1,24 +1,6 @@
 export interface User {
   id: number;
   name: string;
-  username: string;
   email: string;
-  contact: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
   phone: string;
-  website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
 }
